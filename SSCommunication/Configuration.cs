99eb22e79f598abd31eb411c560a613ef988c724
Aman Adlakha <letusgolive@gmail.com>
2022-01-01 01:21:31 +0530

    Updated Config File for Managing Configuration

--- SSCommunication/Configuration.cs
+++ SSCommunication/Configuration.cs
@@ -12,21 +12,10 @@ namespace SSCommunication
         public static IServiceCollection AwsEmailConfigure(this IServiceCollection services, IConfiguration configuration)
         {
             var awsconfig = new AwsConfiguration();
-            configuration.Bind(nameof(AwsConfiguration),awsconfig);
+            configuration.Bind(nameof(SSCommunicationsConfig)+":"+nameof(AwsConfiguration), awsconfig);
             services.AddSingleton<IEmailService>(new AwsEmailServices(awsconfig));
             return services;
         }
-        private static AwsConfiguration GetAwsConfiguration(IConfiguration config)
-        {
-            try
-            {
-                var t = config.GetSection(nameof(AwsConfiguration)) as AwsConfiguration;
-                return t;
-            }
-            catch (Exception ex)
-            {
-                return default;
-            }
-        }
+      
     }
 }
---
+++ SSCommunication/Models/SSCommunicationsConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSCommunication.Models
+{
+    public class SSCommunicationsConfig
+    {
+        public AwsConfiguration AwsConfig = new AwsConfiguration();
+    }
+}
